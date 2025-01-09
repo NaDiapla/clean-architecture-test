44aee80c15268a89876894216f7273d498319013
nadiapla <nadiapla7@gmail.com>
2025-01-09 23:33:09 +0900

    FIX: Fix remote data source

--- data/src/main/java/com/example/data/repository/RemoteDataSource.kt
+++ data/src/main/java/com/example/data/repository/RemoteDataSource.kt
@@ -1,10 +1,13 @@
 package com.example.data.repository
 
+import com.example.domain.entity.BookVolumes
+import kotlinx.coroutines.flow.Flow
+
 interface RemoteDataSource {
-    fun getBooks(
+    suspend fun getBooks(
         query: String,
         printType: String,
         maxResult: Int,
         startIndex: Int
-    )
+    ): BookVolumes
 }
--- data/src/main/java/com/example/data/repository/RemoteDataSourceImpl.kt
+++ data/src/main/java/com/example/data/repository/RemoteDataSourceImpl.kt
@@ -1,12 +1,19 @@
 package com.example.data.repository
 
 import com.example.data.network.bookapi.BookApi
+import com.example.domain.entity.BookVolumes
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class RemoteDataSourceImpl @Inject constructor(
     private val bookApiClient: BookApi
 ): RemoteDataSource {
-    override fun getBooks(query: String, printType: String, maxResult: Int, startIndex: Int) {
-        bookApiClient.getBookVolumeList(query, printType, maxResult, startIndex)
+    override suspend fun getBooks(
+        query: String,
+        printType: String,
+        maxResult: Int,
+        startIndex: Int
+    ): BookVolumes {
+        return bookApiClient.getBookVolumeList(query, printType, maxResult, startIndex)
     }
 }
